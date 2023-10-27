cf2fc732a8baeb947167ece3571623dbf002a481
unknown <MirelaViana.Melo@gmail.com>
2023-10-27 10:25:45 -0300

    Implementação de js no funcionamento de botões e correções de erros no código, add. de um formulário e centralização do site.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+//funcionalidade do botão de habilidades
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if (display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else {
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
